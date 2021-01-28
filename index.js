ff1a066687290eb9f24be28309b99cb6e4c5afe8
xesam <xesamguo@gmail.com>
2021-01-28 16:33:41 +0800

    pkg main

--- index.js
+++
@@ -1,5 +0,0 @@
-const decorate = require('./src/decorate');
-const hook = require('./src/hook');
-
-exports.decorate = decorate;
-exports.hook = hook;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const decorate = require('./decorate');
+const hook = require('./hook');
+
+exports.decorate = decorate;
+exports.hook = hook;
