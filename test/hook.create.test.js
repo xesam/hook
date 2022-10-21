09038adfcb29d40ad7e1296f6fbe3b4cce8a88e6
xesam <xesamguo@gmail.com>
2022-10-21 11:10:07 +0800

    hook.create.test /fix

--- test/hook.create.test.js
+++ test/hook.create.test.js
@@ -1,74 +1,80 @@
 const defaultHook = require('../src/hook');
-const customHook = defaultHook.create('#');
 
-describe('hook attr', () => {
-    it('default hook', () => {
-        const onLoadHook = jest.fn();
-        const targetObj = {
+describe('hook create', () => {
+    let customHook;
+    let rawObj;
+    let onLoadMock;
+    let onLifetimeCreateMock;
+    beforeEach(() => {
+        customHook = defaultHook.create('#');
+        onLoadMock = jest.fn();
+        onLifetimeCreateMock = jest.fn();
+        rawObj = {
+            name: 'rawObj.value',
             data: {
-                name: 'target'
+                name: 'rawObj.data.value'
             },
-            life: {
+            onLoad(a, b) {
+                onLoadMock(this.name, this.data, a, b);
+            },
+            lifetimes: {
+                name: 'lifetimes.value',
                 data: {
-                    name: 'life'
+                    name: 'lifetimes.data.value'
+                },
+                created(a, b) {
+                    onLifetimeCreateMock(this.name, this.data, a, b);
                 }
             }
         };
-        const hooked = defaultHook(targetObj, {
-            'life.onLoad': {
+    })
+    it('when create a new hook then the default hook keep origin', () => {
+        const onMockHook = jest.fn();
+        const hooked = defaultHook(rawObj, {
+            'onLoad': {
                 before(a, b) {
-                    onLoadHook(this.data, a, b);
+                    onMockHook(this.data, a, b);
                 }
             },
 
-            'life.onShow'() {
+            'lifetimes.created'() {
                 return {
                     before(a, b) {
-                        onLoadHook(this.data, a, b);
+                        onMockHook(this.data, a, b);
                     }
                 }
             }
         });
-        hooked.life.onLoad(100, 200);
-        hooked.life.onShow(300, 400);
+        hooked.onLoad(100, 200);
+        hooked.lifetimes.created(300, 400);
 
-        expect(onLoadHook).toBeCalledTimes(2);
-        expect(onLoadHook.mock.calls[0]).toEqual([{name: 'life'}, 100, 200]);
-        expect(onLoadHook.mock.calls[1]).toEqual([{name: 'life'}, 300, 400]);
+        expect(onMockHook).toBeCalledTimes(2);
+        expect(onMockHook.mock.calls[0]).toEqual([{name: 'rawObj.data.value'}, 100, 200]);
+        expect(onMockHook.mock.calls[1]).toEqual([{name: 'lifetimes.data.value'}, 300, 400]);
     });
-    it('custom hook', () => {
-        const onLoadHook = jest.fn();
-        const targetObj = {
-            data: {
-                name: 'target'
-            },
-            life: {
-                data: {
-                    name: 'life'
-                }
-            }
-        };
-        const hooked = customHook(targetObj, {
-            'life#onLoad': {
+    it('when hook with customHook then customHook is applied', () => {
+        const onMockHook = jest.fn();
+        const hooked = customHook(rawObj, {
+            'onLoad': {
                 before(a, b) {
-                    onLoadHook(this.data, a, b);
+                    onMockHook(this.data, a, b);
                 }
             },
 
-            'life#onShow'() {
+            'lifetimes#created'() {
                 return {
                     before(a, b) {
-                        onLoadHook(this.data, a, b);
+                        onMockHook(this.data, a, b);
                     }
                 }
             }
         });
-        hooked.life.onLoad(100, 200);
-        hooked.life.onShow(300, 400);
+        hooked.onLoad(100, 200);
+        hooked.lifetimes.created(300, 400);
 
-        expect(onLoadHook).toBeCalledTimes(2);
-        expect(onLoadHook.mock.calls[0]).toEqual([{name: 'life'}, 100, 200]);
-        expect(onLoadHook.mock.calls[1]).toEqual([{name: 'life'}, 300, 400]);
+        expect(onMockHook).toBeCalledTimes(2);
+        expect(onMockHook.mock.calls[0]).toEqual([{name: 'rawObj.data.value'}, 100, 200]);
+        expect(onMockHook.mock.calls[1]).toEqual([{name: 'lifetimes.data.value'}, 300, 400]);
     });
 });
 
